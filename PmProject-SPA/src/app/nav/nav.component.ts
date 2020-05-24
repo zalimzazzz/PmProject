eb816226b54ca83781a50e3633f915baf5659c10
Chanasit Sangsupamongkol <zalimzazz@hotmail.com>
2020-05-25 00:00:31 +0700

    End of Section 7

--- PmProject-SPA/src/app/nav/nav.component.ts
+++ PmProject-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,8 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-constructor(public authService: AuthService, private alertify: AlertifyService) { }
+constructor(public authService: AuthService, private alertify: AlertifyService,
+  private router: Router) { }
 
   ngOnInit() {
   }
@@ -20,6 +22,8 @@ constructor(public authService: AuthService, private alertify: AlertifyService) 
       this.alertify.success('Logged in successfully');
     }, error => {
       this.alertify.error('Failed to login');
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -30,5 +34,6 @@ constructor(public authService: AuthService, private alertify: AlertifyService) 
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
