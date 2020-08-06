7dd19db70613e379f21944265accb9f6fb1b1d7b
LAPTOP-VAFNT4OT\KO <sarawut@jpwall.com>
2020-08-06 20:50:07 +0700

    update TemplateServiceOrder

--- PmProject-SPA/src/app/template-service-order/template-service-order-add-edit/question-add/question-add.component.ts
+++ PmProject-SPA/src/app/template-service-order/template-service-order-add-edit/question-add/question-add.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-add.component.scss']
 })
 export class QuestionAddComponent implements OnInit {
-
+  question: TemplateServiceOrderQuestion = new TemplateServiceOrderQuestion();
   constructor() { }
 
   ngOnInit() {
+    console.log(this.question);
+    this.question.question = 'Question 1';
+    this.question.answerType = 1;
+    this.question.templateServiceOrderAnswer = [{ id: 1, choice: 'choice1' }, { id: 2, choice: 'choice2' }, { id: 3, choice: 'choice3' }];
   }
 
+  addChoice() {
+    this.question.templateServiceOrderAnswer.push({ id: null, choice: '' })
+  }
+}
+
+export class TemplateServiceOrderQuestion {
+  constructor() { }
+  public question: string;
+  public answerType: number;
+  public templateServiceOrderAnswer: Array<TemplateServiceOrderAnswer>;
+}
+
+export class TemplateServiceOrderAnswer {
+  constructor() { }
+  public id: number;
+  public choice: string;
 }
