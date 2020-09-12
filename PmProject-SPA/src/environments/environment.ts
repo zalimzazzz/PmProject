9ae1000975894838da65f0fd736399191cd8e744
DESKTOP-OF6UFAO\KO <sarawut.klu@gmail.com>
2020-09-12 17:41:51 +0700

    server config

--- PmProject-SPA/src/environments/environment.ts
+++ PmProject-SPA/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:9999/api/'
-  // apiUrl: 'http://localhost:5000/api/'
+  // apiUrl: 'http://localhost:9999/api/'
+  apiUrl: 'http://localhost:5000/api/'
   // apiUrl: 'http://resize.promkai.com/api/api/'
 };
 
